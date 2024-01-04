efe8033525d27272410d4d922e630795889e9827
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2024-01-05 01:13:36 +0900

    feat(ing) : deepRef 설계 고민중
    deepRef 같은 경우 하위 요소 전체를 계속 검사해야되기에 재귀함수를 구현해야될꺼 같은데 답답함

--- practice/클래스 반응성 테스트/js/core/_deepRef.js
+++ practice/클래스 반응성 테스트/js/core/_deepRef.js
@@ -5,8 +5,6 @@ export function shallowRef(initialValue) {
   const state = { value: initialValue };
   return Object.defineProperty({}, "value", {
     get: () => CoreProxyFactory.proxyGetter(state, "value"),
-    set: (value) => {
-      CoreProxyFactory.proxySetter(state, "value", value);
-    },
+    set: (value) => CoreProxyFactory.proxySetter(state, "value", value),
   });
 }
