bd0c3c9e5fcb8aa93854887d701559c88641b67b
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2024-01-05 16:54:09 +0900

    feat : drawing-board canvas 드래그 4분면 좌표 계산
    1: x-  y-
    2: x+ y-
    3: x- y+
    4: x+ y+

--- components/drawing-board/index.vue
+++ components/drawing-board/index.vue
@@ -12,11 +12,50 @@ function useDrawingCanvas(parentElementId: string) {
     const $canvas = document.createElement("canvas");
     const context = $canvas.getContext("2d") as CanvasRenderingContext2D;
 
+    class Position {
+      x: number;
+      y: number;
+      constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+      }
+    }
+
+    const state: {
+      zoom: number;
+      pageX: number;
+      pageY: number;
+      position: Position | null;
+    } = {
+      zoom: 0,
+      pageX: 0,
+      pageY: 0,
+      position: null,
+    };
+
     $canvas.width = $parentElement.offsetWidth;
     $canvas.height = $parentElement.offsetHeight;
     window.addEventListener("resize", function () {
       $canvas.width = $parentElement.offsetWidth;
       $canvas.height = $parentElement.offsetHeight;
+    });
+
+    $canvas.addEventListener("mousedown", function (event) {
+      const [x, y] = [event.offsetX, event.offsetY];
+      state.position = new Position(x, y);
+    });
+    $canvas.addEventListener("mousemove", function (event) {
+      if (!state.position) return;
+
+      const [x, y] = [event.offsetX, event.offsetY];
+      state.pageX += x - state.position.x;
+      state.pageY += y - state.position.y;
+      state.position.x = x;
+      state.position.y = y;
+    });
+    $canvas.addEventListener("mouseup", function () {
+      if (!state.position) return;
+      state.position = null;
     });
 
     $parentElement.append($canvas);
